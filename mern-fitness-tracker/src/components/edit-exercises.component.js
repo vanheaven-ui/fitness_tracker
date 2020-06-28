21a982312f15e91afbd97acdc917d048accec908
vanheaven <vanheaven6@gmail.com>
2020-06-28 09:53:46 +0300

    Add app components

---
+++ mern-fitness-tracker/src/components/edit-exercises.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class EditExercise extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>You are now on the edit exercises component</p>
+      </div>
+    )
+  }
+}
---
+++ mern-fitness-tracker/src/components/exercises-list.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class ExerciseList extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>You are now on the exercises list component</p>
+      </div>
+    )
+  }
+}
